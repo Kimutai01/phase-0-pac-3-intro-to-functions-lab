278b611111a4ed57917ce8cd425fec441075c453
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-08-04 08:02:22 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(text) {
+  return text.toUpperCase();
+}
+function whisper(text) {
+  return text.toLowerCase();
+}
+function logWhisper(text) {
+  console.log(text.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(text) {
+  switch (text) {
+    case text.toUpperCase():
+      return "YES INDEED!";
+      break;
+    case text.toLowerCase():
+      return "I can't hear you!";
+      break;
+    default:
+      return "I would love to!";
+  }
+}
+
+module.exports = {
+  shout,
+  whisper,
+  logWhisper,
+  sayHiToHeadphonedRoommate,
+};
